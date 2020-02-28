5445d39b9877f2904b25a09012bfc6bf2bee0b31
MDFrough <61589320+MDFrough@users.noreply.github.com>
2020-02-28 23:19:33 +0500

    Add files via upload

---
+++ PathFinder.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    class PathFinder
+    {
+        readonly static int B = 4;
+
+        public void WarshallAlgo(int[,] vs) {
+            int[,] matrix = new int[B, B];
+            for (int i = 0; i < B; i++)
+            {
+                for (int j = 0; j < B; j++)
+                {
+                    matrix[i, j] = vs[i, j];
+                }
+            }
+            for (int i = 0; i < B; i++)
+            {
+                for (int j = 0; j < B; j++)
+                {
+                    for (int k = 0; k < B; k++)
+                    {
+                        matrix[j, k] = (matrix[j, k] != 0) || ((matrix[i, k] != 0) && (matrix[j, i] != 0)) ? 1 : 0;
+                    }
+                }
+            }
+
+            matrixPrinter(matrix);
+        }
+        public void matrixPrinter(int[,] vs) {
+            Console.WriteLine("this matrix below shows the minimum distance");
+            for (int i = 0; i < B; ++i)
+            {
+                for (int j = 0; j < B; ++j)
+                {
+                    if (vs[i, j] == 99999)
+                    {
+                        Console.Write("∞\t");
+                    }
+                    else
+                    {
+                        Console.Write(vs[i, j] + "\t");
+                    }
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = {
+                {1, 1, 0, 1},
+                {0, 1, 1, 0},
+                {0, 0, 1, 1},
+                {0, 0, 0, 1}
+            };
+
+            PathFinder obj = new PathFinder();
+
+            // THis is were the magic happens
+            obj.WarshallAlgo(matrix);
+        }
+    }
+}
+
